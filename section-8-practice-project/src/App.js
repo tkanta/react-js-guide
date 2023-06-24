5aa3a15ff53b88b9dd7a78e9c646784e6e0c592e
tarini <tarininegi@gmail.com>
2023-06-24 15:03:25 +0530

    reactjs chpt-9

--- section-8-practice-project/src/App.js
+++ section-8-practice-project/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 import AddUser from './components/Users/AddUser'
 import UserList from './components/Users/UserList'
 
@@ -13,10 +13,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
         <AddUser addUserList={userListHandler}/>
         <UserList userList = {userList}/>
-    </div>
+    </Fragment>
   );
 }
 
--- section-8-practice-project/src/components/Users/AddUser.js
+++ section-8-practice-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, Fragment} from 'react'
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 import Button from '../UI/Button'
@@ -40,7 +40,7 @@ const AddUser = (props) => {
     }
     
     return (
-       <div>
+       <Fragment>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
             <Card className={classes.input}> 
                 <form onSubmit={addUserHandler}>
@@ -51,7 +51,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-       </div> 
+       </Fragment> 
        
         );
 };
